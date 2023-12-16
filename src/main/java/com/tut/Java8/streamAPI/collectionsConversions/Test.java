4a5c28463426c8078efe97a7684618b7e00084a8
Himanshu4256 <mhimanshu2104@gmail.com>
2023-12-17 00:05:21 +0530

    collection conversions
    collection conversions using stream API

--- src/main/java/com/tut/Java8/streamAPI/collectionsConversions/Test.java
+++ src/main/java/com/tut/Java8/streamAPI/collectionsConversions/Test.java
@@ -1,5 +1,25 @@
 package com.tut.Java8.streamAPI.collectionsConversions;
 
-public class Test {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student(101,"Q");
+        Student s2 = new Student(102,"P");
+
+        List<Student> list = new ArrayList<Student>();
+
+        list.add(s1);
+        list.add(s2);
+
+        // converting List to Map Using Streams
+        //Map<id,Student>
+        Map<Integer,Student> map = list.stream().collect(Collectors.toMap(Student::getId, student->student));
+        System.out.println(map);
+
+
+    }
 }
